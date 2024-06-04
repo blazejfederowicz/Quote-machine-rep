e07933351b846a870f2790d3c08f5598d429c6cb
Warrgun <131681381+Warrgun@users.noreply.github.com>
2024-06-04 20:04:06 +0200

    started navbar

---
+++ quote-machine/src/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+const Navbar = () =>{
+    return(
+        <div className="nav-container">
+            <nav className="navbar">
+                <ul className="nav-ul">
+                    <li>Home</li>
+                    <li>Quote Machine</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    )
+} 
+
+export default Navbar
