82266a51c92a40b8fca9bf47b0dd1cf59b068507
Warrgun <131681381+Warrgun@users.noreply.github.com>
2024-06-24 15:07:30 +0200

    gfd

--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const axios = require('axios').default;
+const cors = require('cors')
+
 
 dotenv.config({ path: __dirname + '/secure.env' });
 
 const app = express();
 const port = process.env.PORT || 3000;
 const apiKey = process.env.API_KEY;
+const allowedOrigins = [
+    'quote-machine-rep.vercel.app',
+    'quote-machine-rep-git-main-blazejs-projects-e5b94814.vercel.app',
+    'quote-machine-gmi2fhf5g-blazejs-projects-e5b94814.vercel.app'
+  ];
+
+const corsOptions = {
+    origin: function(origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  };
+
+  app.use(cors(corsOptions));
 
 if (!apiKey) {
     console.error('API_KEY is not defined in the environment variables.');
