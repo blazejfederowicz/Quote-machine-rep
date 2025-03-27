a74e767f69825cef0db67e9dbdcba93d353145b9
Błażej <wargun364@gmail.com>
2025-03-27 15:53:24 +0100

    changed github link

--- quote-machine/src/FooterComponent.jsx
+++ quote-machine/src/FooterComponent.jsx
@@ -5,8 +5,8 @@ const FooterComponent = () =>{
     return(
         <div id="footer" className='footer'>
             <div className="social-container">
-                <a href='https://github.com/Warrgun' className='social-icons' target='_blank'><i className="fa-brands fa-github"></i></a>
-                <a href='https://www.instagram.com/fabio.1130/' className='social-icons' target='_blank'><i className="fa-brands fa-instagram"></i></a>
+                <a href='https://github.com/blazejfederowicz' className='social-icons' target='_blank'><i className="fa-brands fa-github"></i></a>
+                <a href='https://www.instagram.com/' className='social-icons' target='_blank'><i className="fa-brands fa-instagram"></i></a>
                 <a href='https://x.com' className='social-icons' target='_blank'><i className="fa-brands fa-x-twitter"></i></a>
             </div>
         </div>
